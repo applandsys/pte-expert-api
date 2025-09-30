aa5e5fc57054aa1fdb8676ebbaacc7f1c9522db6
Tarique Mosharraf <applandsys@gmail.com>
2025-09-30 11:01:36 +0600

    Web route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            VerifyCsrfToken::class,
+        ]);
+    })
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ Route::get('/', function () {
     return "PTE EXPERT BACKEND";
 });
 
-
-
 Route::post('/payment/initiate', [PaymentController::class, 'initiate']);
 Route::post('/payment/success', [PaymentController::class, 'success'])->name('payment.success');
 Route::post('/payment/fail', [PaymentController::class, 'fail'])->name('payment.fail');
